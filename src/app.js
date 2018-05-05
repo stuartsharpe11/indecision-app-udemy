9b44fe52420c811c18dbf354e78f299e2fccee6c
Stuart Sharpe <stuart.a.sharpe@gmail.com>
2018-05-05 15:09:01 -0700

    Section 3, Lecture 20 Complted

--- src/app.js
+++ src/app.js
@@ -24,9 +24,13 @@ const onRemoveAll = () => {
     render();
 };
 
-const appRoot = document.getElementById('app');
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
 
-// const numbers = [55, 101, 2000];
+const appRoot = document.getElementById('app');
 
 const render = () => {
     const template = (
@@ -34,13 +38,8 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick = {onMakeDecision}>What should I do?</button>
             <button onClick = {onRemoveAll}>Remove All</button>
-            {
-               /* numbers.map((number) => {
-                    return <p key={number}>Number: {number}</p>;
-                }) */
-            }
             <ol>
                 {
                     app.options.map((option) => {
